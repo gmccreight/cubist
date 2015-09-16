80c83bb0fe5343858289e073c6044cb796536ebc
Gordon McCreight <gordon@mccreight.com>
2015-09-16 09:20:38 -0700

    move more stuff to helper

--- spec/source_files_finder_spec.rb
+++ spec/source_files_finder_spec.rb
@@ -10,17 +10,6 @@ describe Cubist::SourceFilesFinder do
     @finder = Cubist::SourceFilesFinder.new(root: cubist_dir)
   end
 
-  def file_and_link(fullpath, linkpath)
-    file_full_path = project_dir + "/" + fullpath
-    link_full_path = cubist_dir + "/" + linkpath
-
-    FileUtils.mkdir_p(Pathname.new(file_full_path).dirname.to_s)
-    FileUtils.mkdir_p(Pathname.new(link_full_path).dirname.to_s)
-
-    FileUtils.touch(file_full_path)
-    File.symlink(file_full_path, link_full_path)
-  end
-
   it "should get all the files" do
     file_and_link("app/models/item.rb", "availability/models/item.rb")
     expect(@finder.run.size).to eq(1)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,22 @@ def cubist_dir
   Dir.mkdir(@cubist_dir)
   @cubist_dir
 end
+
+def make_project_file(full_path, content: "")
+  file_full_path = project_dir + "/" + full_path
+  FileUtils.mkdir_p(Pathname.new(file_full_path).dirname.to_s)
+  FileUtils.touch(file_full_path)
+  file_full_path
+end
+
+def make_cubist_link(project_file_full_path, cubist_link_path)
+  cubist_link_full_path = cubist_dir + "/" + cubist_link_path
+  FileUtils.mkdir_p(Pathname.new(cubist_link_full_path).dirname.to_s)
+  File.symlink(project_file_full_path, cubist_link_full_path)
+end
+
+def file_and_link(project_file_path, cubist_link_path)
+  project_file_full_path = make_project_file(project_file_path)
+  make_cubist_link(project_file_full_path, cubist_link_path)
+end
+
