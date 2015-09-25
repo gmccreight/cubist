37e8bc83fcb6200165e86032247b635ef19f2833
Gordon McCreight <gordon@mccreight.com>
2015-09-25 09:46:43 -0700

    refactor a little bit

--- spec/cubist_spec.rb
+++ spec/cubist_spec.rb
@@ -5,18 +5,18 @@ describe Cubist do
     expect(Cubist::VERSION).not_to be nil
   end
   describe 'cli' do
-    def run(dir, command)
-      `ruby -Ilib ./exe/cubist --dir=#{dir} #{command}`
+    def run(command)
+      `ruby -Ilib ./exe/cubist --dir=#{temp_dir} #{command}`
     end
     describe 'show help' do
       it 'when it gets the right flags' do
-        expect(run(temp_dir, '-h')).to match(/Usage:/)
-        expect(run(temp_dir, '--help')).to match(/Usage:/)
+        expect(run('-h')).to match(/Usage:/)
+        expect(run('--help')).to match(/Usage:/)
       end
     end
     describe 'make a perspective' do
       it 'when it gets the right flags' do
-        run(temp_dir, '--make_perspective=features/coupons')
+        run('--make_perspective=features/coupons')
         expect(`find #{temp_dir}`).to match(/features\/coupons\/.cubist_perspective/)
       end
     end
