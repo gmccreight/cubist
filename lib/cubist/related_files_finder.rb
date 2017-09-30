85aa6d941cc6241b682d07f138af500f3d3aa7ff
Gordon McCreight <gordon@mccreight.com>
2017-09-30 10:15:34 -0700

    calculate the best related files for all files passed in

--- lib/cubist/related_files_finder.rb
+++ lib/cubist/related_files_finder.rb
@@ -14,13 +14,19 @@ module Cubist
 
     def find(files:, commits:)
       result = {all: nil, per_file: {}}
+      global_score_for = {}
+      global_score_for.default = 0
       files.each do |file|
         files_and_scores = _related_files(
           file: file,
           commits: commits
         )
+        files_and_scores.each do |file_and_score|
+          global_score_for[file_and_score[0]] += file_and_score[1]
+        end
         result[:per_file][file] = files_and_scores.map{|x| x[0]}
       end
+      result[:all] = global_score_for.sort_by{ |k, v| v }.reverse.map{|x| x[0]}
       result
     end
 
--- spec/related_files_finder_spec.rb
+++ spec/related_files_finder_spec.rb
@@ -34,39 +34,83 @@ describe Cubist::RelatedFilesFinder do
     Cubist::Commit.new(sha: nil, files: files)
   end
 
-  def expect_per_file_for(filename, expected_files)
-    files = @finder.find(files: [filename], commits: @commits)
-    per_file = files[:per_file][filename]
-    expect(per_file).to eq(expected_files)
-  end
-
-  it "should get all related files ranked by the number of times they have been committed together" do
-    expect_per_file_for(
-      "app/models/item.rb", 
-      [
-        "spec/models/item_spec.rb",
-        "app/models/item_image.rb",
-        "app/models/product.rb"
-      ]
-    )
-
-    10.times {
-      @commits << commit([
+  describe "all" do
+    it "should get the top ranked files by all connections between all files" do
+      commits = []
+      commits << commit([
+        "app/models/item.rb",
+        "spec/models/item_spec.rb"
+      ])
+      commits << commit([
         "app/models/item.rb",
         "app/models/product.rb",
+        "spec/models/item_spec.rb"
       ])
-    }
-
-    expect_per_file_for(
-      "app/models/item.rb", 
-      [
+      commits << commit([
         "app/models/product.rb",
-        "spec/models/item_spec.rb",
-        "app/models/item_image.rb"
-      ]
-    )
+        "spec/models/product_spec.rb"
+      ])
+      commits << commit([
+        "app/models/product.rb",
+        "spec/models/product_spec.rb"
+      ])
+
+      files = @finder.find(
+        files: ["spec/models/item_spec.rb", "spec/models/product_spec.rb"],
+        commits: commits
+      )
+      expect(files[:all].first(2)).to eq(["app/models/product.rb", "app/models/item.rb"])
+
+      2.times do
+        commits << commit([
+          "app/models/item.rb",
+          "spec/models/item_spec.rb"
+        ])
+      end
+
+      files = @finder.find(
+        files: ["spec/models/item_spec.rb", "spec/models/product_spec.rb"],
+        commits: commits
+      )
+      expect(files[:all].first(2)).to eq(["app/models/item.rb", "app/models/product.rb"])
+
+    end
   end
-  it "should work with an alternative set of data" do
-    expect_per_file_for( "spec/models/store_spec.rb", ["app/models/store.rb"])
+
+  describe "per_file" do
+    def expect_per_file_for(filename, expected_files)
+      files = @finder.find(files: [filename], commits: @commits)
+      per_file = files[:per_file][filename]
+      expect(per_file).to eq(expected_files)
+    end
+    it "should get all related files ranked by the number of times they have been committed together" do
+      expect_per_file_for(
+        "app/models/item.rb", 
+        [
+          "spec/models/item_spec.rb",
+          "app/models/item_image.rb",
+          "app/models/product.rb"
+        ]
+      )
+
+      10.times {
+        @commits << commit([
+          "app/models/item.rb",
+          "app/models/product.rb",
+        ])
+      }
+
+      expect_per_file_for(
+        "app/models/item.rb", 
+        [
+          "app/models/product.rb",
+          "spec/models/item_spec.rb",
+          "app/models/item_image.rb"
+        ]
+      )
+    end
+    it "should work with an alternative set of data" do
+      expect_per_file_for( "spec/models/store_spec.rb", ["app/models/store.rb"])
+    end
   end
 end
